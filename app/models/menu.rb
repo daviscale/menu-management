7e6baf8493c30994974d37aea69562b9230e0992
Cale Davis <daviscale@gmail.com>
2023-05-21 16:57:42 -0400

    Level 1: annotate menu.rb and menu_spec.rb
    Annotation generated with:
    
    docker-compose run --rm web annotate --models

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: menus
+#
+#  id          :bigint           not null, primary key
+#  name        :text
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Menu < ApplicationRecord
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: menus
+#
+#  id          :bigint           not null, primary key
+#  name        :text
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
