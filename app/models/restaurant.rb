6c8991d8fd6f1e71706409a52e6a6185b114675e
Cale Davis <daviscale@gmail.com>
2023-05-22 23:11:37 -0400

    Level 2: annotate restaurant models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id         :bigint           not null, primary key
+#  name       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Restaurant < ApplicationRecord
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id         :bigint           not null, primary key
+#  name       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
